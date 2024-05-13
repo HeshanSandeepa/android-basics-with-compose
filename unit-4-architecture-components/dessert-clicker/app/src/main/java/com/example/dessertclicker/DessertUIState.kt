d43a097e319a97ec29e42282d75ed6c458d138f9
HeshanSandeepa <ea.heshan@gmail.com>
2024-05-13 10:31:21 +0530

    proceed

--- unit-4-architecture-components/dessert-clicker/app/src/main/java/com/example/dessertclicker/DessertUIState.kt
+++ unit-4-architecture-components/dessert-clicker/app/src/main/java/com/example/dessertclicker/DessertUIState.kt
@@ -3,6 +3,5 @@ package com.example.dessertclicker
 data class DessertUIState(val revenue: Double = 0.0,
                           val currentDessertPrice: Double = 0.0,
                           val dessertsSold: Int = 0,
-                          val currentDessertImageId: Int = 0,
-                          val currentDessertIndex: Int = 0,) {
-}
+                          val currentDessertImageId: Int = R.drawable.cupcake,
+                          val currentDessertIndex: Int = 0,)
