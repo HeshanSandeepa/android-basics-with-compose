bcfab541cd705e26257f6a3fd18be87f46b65a44
heshan_sandeepa <heshan.sandeepa@freshmilksoftware.com>
2024-06-11 17:22:40 +0530

    proceed racetracker

--- unit-5-connecting-to-internet/sync_async/src/Main.kt
+++ unit-5-connecting-to-internet/sync_async/src/Main.kt
@@ -1,24 +1,42 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import kotlinx.coroutines.*
+import kotlinx.coroutines.NonCancellable.cancel
 import kotlin.system.*
 
 fun main() {
     val time = measureTimeMillis {
         runBlocking {
             println("Weather forecast")
-            printForecast()
-            printTemperature()
+            println(getWeatherReport())
+            println("Have a good day at last!")
         }
+        println("Finished")
     }
     println("Execution time: ${time / 1000.0} seconds")
 }
-suspend fun printForecast() {
-    delay(1000)
-    println("Sunny")
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async {
+        try {
+            getTemperature()
+        } catch (e: AssertionError) {
+            println("Caught exception $e")
+            "{ No temperature found }"
+        }
+    }
+    forecast.cancel()
+    "${forecast.await()} ${temperature.await()}"
 }
 
-suspend fun printTemperature() {
+suspend fun getForecast(): String {
     delay(1000)
-    println("30\u00b0C")
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(1000)
+    throw AssertionError("Temperature is invalid")
+    return "30\u00b0C"
 }
